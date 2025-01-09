8b2996af40c800e57b51119f33fb7f6a09dee829
NathanRusso <nathandanielrusso@gmail.com>
2025-01-08 19:25:59 -0500

    I updated the About and Instructions pages.

--- JS/canvas.js
+++ JS/canvas.js
@@ -110,13 +110,13 @@ console.log(mazeCanvas.height);*/
 const keyUpHandler = (event) => { movePlayer(event.key); };
 
 // Anonymous functions for d-pad input 
-const clickHandlerUp = (event) => { console.log("UP"); movePlayer("ArrowUp"); };
-const clickHandlerDown = () => { console.log("DOWN"); movePlayer("ArrowDown"); };
-const clickHandlerLeft = () => { console.log("LEFT"); movePlayer("ArrowLeft"); };
-const clickHandlerRight = () => { console.log("RIGHT"); movePlayer("ArrowRight"); };
+const clickHandlerUp = () => { movePlayer("ArrowUp"); };
+const clickHandlerDown = () => { movePlayer("ArrowDown"); };
+const clickHandlerLeft = () => { movePlayer("ArrowLeft"); };
+const clickHandlerRight = () => { movePlayer("ArrowRight"); };
 
 // Ensure imageMapResize is called after the page has loaded
-window.addEventListener("load", function() {
+window.addEventListener("load", function() { 
     imageMapResize();
 });
 
